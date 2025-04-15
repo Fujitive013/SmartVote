83435428bebf3ec56c04470a7cf7597597f5333c
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-04-15 22:31:17 +0800

    styles: make the fontsize to 22

--- src/styles/candidateStyles.js
+++ src/styles/candidateStyles.js
@@ -25,7 +25,7 @@ const candidateStyles = StyleSheet.create({
   subHeaderText: {
     fontFamily: "Montserrat-Bold",
     color: "#111B56",
-    fontSize: 24,
+    fontSize: 22,
   },
   backImage: {
     width: height * 0.045,
