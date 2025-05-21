76b73c53b21cc5260a05b45e98cb3b0f68d0c4a0
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-05-21 22:34:49 +0800

    refactor: added a navigation to Vote Details

--- src/components/Home/VoteCounter.js
+++ src/components/Home/VoteCounter.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { homeStyles as styles } from "../../styles/HomeStyles";
+import { useNavigation } from "@react-navigation/native";
 
-const VoteCounter = ({ voteCount, totalElections }) => (
-  <View style={styles.voteCounterContainer}>
-    <Text style={styles.voteText}>
-      {voteCount === totalElections
-        ? "You have voted in all ongoing elections."
-        : "You haven't voted in some ongoing elections."}
-    </Text>
-    <View style={styles.countVote}>
-      <Text style={styles.votedText}>{voteCount}</Text>
-      <Text style={styles.overVotedText}> / {totalElections}</Text>
+const VoteCounter = ({ voteCount, totalElections }) => {
+  const navigation = useNavigation();
+
+  const handleVoteDetailsPress = () => {
+    navigation.navigate("Vote Details");
+  };
+
+  return (
+    <View style={styles.voteCounterContainer}>
+      <Text style={styles.voteText}>
+        {voteCount === totalElections
+          ? "You have voted in all ongoing elections."
+          : "You haven't voted in some ongoing elections."}
+      </Text>
+      <View style={styles.countVote}>
+        <Text style={styles.votedText}>{voteCount}</Text>
+        <Text style={styles.overVotedText}> / {totalElections}</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.viewDetailsContainer}
+        onPress={handleVoteDetailsPress}
+      >
+        <Text style={styles.detailsText}>View Details</Text>
+      </TouchableOpacity>
     </View>
-    <TouchableOpacity style={styles.viewDetailsContainer}>
-      <Text style={styles.detailsText}>View Details</Text>
-    </TouchableOpacity>
-  </View>
-);
+  );
+};
 
 export default VoteCounter;
